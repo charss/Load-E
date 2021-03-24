a72842036a1e509aecc5b2b4c091f4b6d7ce54b9
charss <charleskylereyes16@gmail.com>
2021-03-24 15:42:13 +0800

    Added navigation buttons in register or load screen

---
+++ loading_option.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="test">
+            <form method='post'>
+                <div class='topnav'> 
+                    <input type='submit' name='regular' value='Regular Load'>
+                    <input type='submit' name='promo' value='Promos'>
+
+                    <!-- <a class='active' href='#home'>Regular Load</a>
+                    <a href="#news">Promos</a> -->
+                </div>
+            </form>
+            <?php  
+                if(isset($_POST['promo'])) {
+                    include 'promo_screen.html';
+                } else if(isset($_POST['regular'])) {
+                    include 'regular_screen.html';
+                }
+            ?>
+        </div>
+        
+    </body>
+</html>
