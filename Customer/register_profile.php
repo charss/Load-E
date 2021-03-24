140fbdd22264afda515c5da6ac39f6e57183e52e
charss <charleskylereyes16@gmail.com>
2021-03-24 22:55:40 +0800

    Added registration function

--- Customer/register_profile.php
+++ Customer/register_profile.php
@@ -35,9 +35,22 @@
                             die('Connection failed: ' . $conn->connect_error);
                         }
 
-                        $sql = "SELECT username FROM users"
-
+                        $sql = "SELECT name FROM users WHERE username='$user'";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            echo "<script>alert('Username already exist')";
+                        } else {
+                            $sql = "INSERT INTO users (name, pass, num) VALUES ('$user', '$pass', '$number')";
+                            if ($conn->query($sql) === TRUE) {
+                                echo "<script>alert('New record created successfully')</script>";
+                            } else {
+                                echo "<script>alert('Error')</script>";
+                                // echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                        }
                     }
+
+                    $conn->close();
                 }
                 
                 
